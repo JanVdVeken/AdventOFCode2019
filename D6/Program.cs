2ac24b30a350b26df08165e99a99046fc5e3c307
Jan Van der Veken <vekie1@hotmail.com>
2019-12-11 22:36:30 +0100

    Start dag 6

---
+++ D6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace D6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+             //input
+            string[] inputFile = File.ReadAllLines(@"..\OpdrachtGegevens\D6O1_test.txt");
+            List<SpaceObject> spaceObjects = new List<SpaceObject>();
+            
+            //Verwerlomg
+            foreach(string lijn in inputFile)
+            {
+                var inputLijn = lijn.Split(")");
+
+            };        
+        
+           
+        }
+    }
+}
---
+++ D6/SpaceObject.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace D6
+{
+    class SpaceObject
+    {
+        string name;
+        List<SpaceObject> spaceObjects ;
+        public SpaceObject(string name)
+        {
+            
+        }
+
+
+    }
+}
